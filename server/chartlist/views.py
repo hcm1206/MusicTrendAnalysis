55b4688cb5481e7aa8af2ee290f67d2e5c27bf2d
hcm1206 <hcm1206@naver.com>
2024-10-15 16:45:33 +0900

    차트 날짜 변경 기능 추가, 개별 곡 세부 정보 및 역대 차트 출력 기능 추가(beta)

--- server/chartlist/views.py
+++ server/chartlist/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import Http404
 from datetime import datetime, timedelta
 import pandas as pd
+import numpy as np
+import json
 
 def chartlistPage(request, date):
     try:
@@ -15,9 +17,9 @@ def chartlistPage(request, date):
     while checkday.weekday() < 6:
         checkday -= timedelta(days=1)
 
-    data = pd.read_csv("../data/final_total_data.csv", index_col=0)
+    data = pd.read_csv("../modeling/00_projectx/test/circle_chart_with_trends(20241002).csv")
     data = data[data["date"] == checkday.strftime('%Y-%m-%d')]
-    data = data[["Rank", "Title", "New_Artist", "Production", "Genre"]]
+    data = data[["Rank", "Title", "Artist"]]
 
     weekday_list = ['일', '월', '화', '수', '목', '금', '토']
 
@@ -45,11 +47,47 @@ def chartSongPage(request, date, rank):
     while checkday.weekday() < 6:
         checkday -= timedelta(days=1)
 
+    data = pd.read_csv("../modeling/00_projectx/test/circle_chart_with_trends(20241002).csv")
+
+    weekly_info = data[(data["date"] == checkday.strftime('%Y-%m-%d')) & (data["Rank"] == int(rank))].iloc[0].replace({np.nan: None})
+
+    title = weekly_info["Title"]
+    artist = weekly_info["Artist"]
+    production = weekly_info["Production"]
+    genre = weekly_info["Genre"]
+    runtime = weekly_info["Runtime"]
+    ky_rank = weekly_info["ky_rank"]
+
+    prev_rank_list = []
+    prev_date_list = []
+
+    date_now = checkday
+    week_cnt = 0
+
+    while datetime(year=2023, month=9, day=24) <= date_now and week_cnt <= 52:
+        prev_date_list.append(date_now.strftime('%Y-%m-%d'))
+        rank_now = data[(data["Title"] == title) & (data["Artist"] == artist) & (data["date"] == date_now.strftime('%Y-%m-%d'))][["Rank"]].iloc[0].values
+        prev_rank_list.append(rank_now)
+        week_cnt += 1
+
+    print(prev_rank_list)
+    print(prev_date_list)
+    prev_ranks = data[(data["Title"] == title) & (data["Artist"] == artist)][["date", "Rank"]]
+    prev_ranks_list = prev_ranks.to_numpy().tolist()
+    prev_ranks_list_json = json.dumps(prev_ranks_list).replace('None', 'null')
+
     content = {
         'year' : checkday.year,
         'month' : checkday.month,
         'day' : checkday.day,
         'rank' : rank,
+        'title' : title,
+        'artist' : artist,
+        'production' : production,
+        'genre' : genre,
+        'runtime' : runtime,
+        'ky_rank' : ky_rank,
+        'ranks' : prev_ranks_list_json,
     }
 
     return render(request, 'chartlist/chartsong.html', content)
