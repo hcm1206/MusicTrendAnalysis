6345db56ab8bb5fa21a3c4bff73c5b7615bc7388
hcm1206 <hcm1206@naver.com>
2024-10-15 10:59:41 +0900

    차트별 곡 세부 페이지 URL 라우팅

--- server/chartlist/urls.py
+++ server/chartlist/urls.py
@@ -7,5 +7,6 @@ app_name = "chartlistapp"
 
 urlpatterns = [
     path("", RedirectView.as_view(url='2024-09-08')), # 차트리스트 메인 페이지
-    re_path(r'^(\d{4}-\d{2}-\d{2})$', views.chartlistPage)
+    re_path(r'^(\d{4}-\d{2}-\d{2})$', views.chartlistPage),
+    re_path(r'^(\d{4}-\d{2}-\d{2})/([1-9][0-9]{0,1}|1[0-9]{2}|200)$', views.chartSongPage)
 ]
--- server/chartlist/views.py
+++ server/chartlist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from datetime import datetime, timedelta
+import pandas as pd
 
 def chartlistPage(request, date):
     try:
@@ -11,18 +12,44 @@ def chartlistPage(request, date):
     checkday = min(checkday, datetime(year=2024, month=9, day=8))
     checkday = max(checkday, datetime(year=2023, month=9, day=24))
 
-    print(checkday.weekday())
-
-    while checkday.weekday() > 0:
+    while checkday.weekday() < 6:
         checkday -= timedelta(days=1)
 
+    data = pd.read_csv("../data/final_total_data.csv", index_col=0)
+    data = data[data["date"] == checkday.strftime('%Y-%m-%d')]
+    data = data[["Rank", "Title", "New_Artist", "Production", "Genre"]]
+
     weekday_list = ['일', '월', '화', '수', '목', '금', '토']
+
+    content = {
+        'date' : checkday.strftime('%Y-%m-%d'),
+        'year' : checkday.year,
+        'month' : checkday.month,
+        'day' : checkday.day,
+        'weekday' : weekday_list[checkday.weekday()],
+        'chart_data' : data.values.tolist(),
+    }
+
+    return render(request, 'chartlist/chartlist.html', content)
+
+
+def chartSongPage(request, date, rank):
+    try:
+        checkday = datetime.strptime(date, '%Y-%m-%d')
+    except:
+        raise Http404("Invalid Date; 날짜 형식이 올바르지 않습니다.")
+    
+    checkday = min(checkday, datetime(year=2024, month=9, day=8))
+    checkday = max(checkday, datetime(year=2023, month=9, day=24))
+
+    while checkday.weekday() < 6:
+        checkday -= timedelta(days=1)
 
     content = {
         'year' : checkday.year,
         'month' : checkday.month,
         'day' : checkday.day,
-        'weekday' : weekday_list[checkday.weekday()],
+        'rank' : rank,
     }
 
-    return render(request, 'chartlist/chartlist.html', content)
+    return render(request, 'chartlist/chartsong.html', content)
