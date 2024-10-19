b6116ec5b1dd7654ccf357a9709898799bc211a3
hcm1206 <hcm1206@naver.com>
2024-10-19 21:17:46 +0900

    웹 프론트 메인페이지 디자인 적용, 기타 디자인 일부 변경

--- server/static/js/mainPage.js
+++ server/static/js/mainPage.js
@@ -1,16 +1,6 @@
 // 메인 페이지 자바스크립트 파일
 
-// 메인 페이지의 버튼 클릭하면 차트 페이지로 넘어가는 이벤트리스너 설정
-document.getElementById("chartbutton").addEventListener("click", (e) => {
-  location.href = '/chart';
-});
-
-// 메인 페이지의 버튼 클릭하면 테스트 페이지로 넘어가는 이벤트리스너 설정
-document.getElementById("testbutton").addEventListener("click", (e) => {
-  location.href = '/test';
-});
-
 // 메인 페이지의 버튼 클릭하면 차트 리스트 페이지로 넘어가는 이벤트리스너 설정
-document.getElementById("chartlistbutton").addEventListener("click", (e) => {
+document.getElementById("link-btn").addEventListener("click", (e) => {
   location.href = '/chartlist';
 });
